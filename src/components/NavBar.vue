9e965dbb9a4a04fbce89d3acef57152689b868b8
zhouyu <1512450002@qq.com>
2018-12-04 19:56:54 +0800

    home的生命周期测试

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,13 +1,33 @@
 <template>
-
+    <div class="navBar">
+        <div class="leftItem">
+        
+        </div>
+        <div class="titleView">
+            <span></span>
+        </div>
+        <div class="rightItem">
+        
+        </div>
+    </div>
 </template>
 
 <script>
 	export default {
-		name: "NavBar"
+		name: "NavBar",
+        props: {
+			title: String
+        }
 	}
 </script>
 
 <style scoped>
-
+    .navBar {
+        height: 44px;
+        background-color: #efefef;
+        position: absolute;
+        top: 0px;
+        left: 0px;
+        right: 0px;
+    }
 </style>
--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -6,7 +6,37 @@
 
 <script>
     export default {
-        name: "Home"
+        name: "Home",
+        beforeCreate() {
+	        console.log('home beforeCreate')
+        },
+        created() {
+        	console.log('home created')
+        },
+        beforeMount() {
+	        console.log('home beforeMount')
+        },
+        mounted() {
+	        console.log('home mounted')
+        },
+        beforeUpdate() {
+	        console.log('home beforeUpdate')
+        },
+        updated() {
+	        console.log('home updated')
+        },
+        activated() {
+	        console.log('home activated')
+        },
+        deactivated() {
+	        console.log('home deactivated')
+        },
+        beforeDestroy() {
+	        console.log('home beforeDestroy')
+        },
+        destroyed() {
+	        console.log('home destroyed')
+        }
     }
 </script>
 
